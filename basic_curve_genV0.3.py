e1bcc3210ba6c1fdc417f2c014aef07536620d90
Chetan Mandloi <chetan.mandloi@live.com>
2018-03-27 19:47:25 +0530

    Third Edition

--- basic_curve_genV0.3.py
+++ basic_curve_genV0.3.py
@@ -1,9 +1,11 @@
 from astropy.io import fits
+from os import listdir
+from os.path import isfile, join
 
 
 def print_data(data):        # Have to add frame select ability later. Will need to see how to give default parameters
     """
-    Prints the whole data in all/selected frame in the console
+    Prints the whole data in all/selected frame(s) in the console
     :param data: The data cube in question
     :return: NULL doesn't return anything, just prints in console
     """
@@ -27,7 +29,6 @@ def get_bright_pixel_intensity(data):
             for k in range(data.shape[2]):
                 if data[i][j][k] > maximum:
                     maximum = data[i][j][k]
-                    maximum_location = [j, k]
         bright_pixel_intensity.append(maximum)
     return bright_pixel_intensity
 
@@ -53,19 +54,27 @@ def get_bright_pixel_location(data):
 def get_bright_pixel_time(head):
     """
     Gives time of observation for each frame
-    :param data: The data cube to be processed
+    :param head: Heaader part of data cube to be processed
     :return: An array holding UTCS Observation times of each frame
     """
     time_obs = []
     for i in range(head['NAXIS3']):
-        time_obs.append(head['UTCS_OBS'] + i * head['FRAMTIME'])
+        time_obs.append(head['UTCS_OBS'] + i * head['FRAMTIME'])  # Assuming Delay between frames is totally dependent on FRAMTIME. Will need to check with mentor
     return time_obs
 
-fin = fits.open('/home/tachyon/Lab/PreCode/datafiles/SPITZER_I2_46466816_0000_0000_2_bcd.fits')
-data_handler = fin[0].data
-print_data(data_handler)
-print "\n\n"
-print "Bright Pixel Address:", get_bright_pixel_location(data_handler)
-print "Bright Pixel Intensities:", get_bright_pixel_intensity(data_handler)
-print "Bright Pixel Times:", get_bright_pixel_time(fin[0].header)
+path = "/home/tachyon/Lab/PreCode/datafiles/"                    # Will get all the files within the given directory
+only_files = [f for f in listdir(path) if isfile(join(path, f))]
+
+fin = []
+for i in range(len(only_files)):
+    fin.append(fits.open(path+only_files[i]))
+
+data_handler = []
+for i in range(len(only_files)):
+    #data_handler.append(fin[i][0].data)
+    print "Cube Name:", only_files[i], "Cube No.: ", i
+    print "Peak intensities: ", get_bright_pixel_intensity(fin[i][0].data)
+    print "Peak locations: ", get_bright_pixel_location(fin[i][0].data)
+    print "Peak time:", get_bright_pixel_time(fin[i][0].header)
+    print "\n\n"
 
